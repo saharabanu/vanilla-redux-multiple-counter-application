4d459996c6c0581d9f8055e708853afc28f77deb
saharabanu <saharabanungn@gmail.com>
2022-08-18 17:50:42 +0600

    multiple counter functionality added

--- script.js
+++ script.js
@@ -1,131 +1,245 @@
-const counterEl = document.getElementById("counter");
-const incrementEl = document.getElementById("increment");
-const decrementEl = document.getElementById("decrement");
 const resetEl = document.getElementById("reset");
 const addCounterEl = document.getElementById("addCounter");
 const fullCounterEl = document.getElementById("full-counter");
 
-
-function addFunction (){
-    const divElem= document.createElement("div");
-
-   divElem.innerHTML =` <div class="max-w-md mx-auto mt-10 space-y-5" id="full-counter">
-    <div
-        class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
-    >
-        <div class="text-2xl font-semibold" id="counter">0</div>
-        <div class="flex space-x-3">
-            <button
-                class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-                id="increment"
-            >
-                Increment
-            </button>
-            <button
-                class="bg-red-400 text-white px-3 py-2 rounded shadow"
-                id="decrement"
-            >
-                Decrement
-            </button>
-
-           
-        </div>
-    </div>
-    
-    
-   
-</div>`;
-    // divElem.appendChild(allElm);
-    document.getElementById("full-counter").appendChild(divElem)
-
-}
-
 // action identifiers 
-
+const ADD_COUNTER='add_counter';
 const INCREMENT ='increment';
 const DECREMENT ='decrement';
 const RESET ='reset';
 
+
+
 // action creators  
 
+const addCounter = () =>{
+    return {
+        type: ADD_COUNTER,
+        payload: {
+          id: store.getState().totalCounters,
+          value: 0,
+          increment: Math.floor(Math.random() * 10) + 1,
+          decrement: Math.floor(Math.random() * 10) + 1,
+        },
+      };
 
-const increment= (value)=>{
+}
+const incrementAction= (id)=>{
     return{
         type:INCREMENT,
-        payload:value
+        payload:{
+            id:id,
+        }
     }
 }
-const decrement= (value)=>{
+const decrementAction= (id)=>{
     return{
         type:DECREMENT,
-        payload:value
+        payload:{
+            id:id
+        }
     }
 }
-const reset= (value)=>{
+const reset= ()=>{
     return{
         type:RESET,
-        payload:value
+       
     }
 }
+
 
 // initial state 
 
-const initialState = {
-    value:2
-}
+let initialState = {
+    counters:[
+       { 
+        id: 0,
+        value: 0,
+        increment: Math.floor(Math.random() * 10) + 1,
+        decrement: Math.floor(Math.random() * 10) + 1,
+       }
 
-// create reducer function  
+    ],
+    totalCounters : 1
+    
+    
+};
 
-function counterReducer (state= initialState,action){
-    if(action.type === INCREMENT){
-            return{
-                ...state,
-                value: state.value + action.payload
-            }
-        
-
-        
-    }
-
-    else if(action.type === DECREMENT){
-        if(state.value === 0){
-            return {
-                value:0
-            }
-         }
-         else{
-            return{
-                ...state,
-                value: state.value - action.payload
-            
-           
-        }
-         }
-        
-    }
-   else if(action.type === RESET){
-    return{
-        value:action.payload
-    }
-
-   }
-    else{
-        return state
-    }
-
-}
 
 // create store  
 
 const store = Redux.createStore(counterReducer);
 
-const render = ()=>{
-    const state = store.getState();
-    counterEl.innerText = state.value.toString();
-}
-render()
 
-store.subscribe(render)
+// add new counter  
+const  addNewCounter = ()=>{
+    const divElem= document.createElement("div");
+    store.getState().counters.forEach((counter) =>{
+        divElem.innerHTML =`<div
+         
+        class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
+      >
+        <div 
+          id="${counter.id + 1}"
+        class="text-2xl font-semibold">
+          ${counter.value}
+        </div>
+        <div class="flex space-x-3">
+          <button
+              id="increment-${counter.id}"
+              onclick="increment('${counter.id + 1}', '${
+                counter.increment
+  }')"
+           class="bg-indigo-400 text-white px-3 py-2 rounded shadow">
+            Increment By ${counter.increment}
+          <button 
+              id="decrement-${initialState.counters + 1}"
+              onclick="decrement('${counter.id + 1}', '${
+    counter.decrement
+  }')"
+          class="bg-red-400 text-white px-3 py-2 rounded shadow">
+            Decrement By ${counter.decrement}
+          </button>
+        </div>
+      </div>
+  `;
+
+    })
+
+   
+    // divElem.appendChild(allElm);
+    fullCounterEl.appendChild(divElem)
+
+}
+
+const resetAllCounter = () => {
+    fullCounterEl.innerHTML = "";
+    store.getState().counters.map((counter) => {
+      const newCounter = document.createElement("div");
+      newCounter.innerHTML = `
+     <div
+           
+            class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
+          >
+            <div 
+              id="${counter.id + 1}"
+            class="text-2xl font-semibold">
+              ${counter.value}
+            </div>
+            <div class="flex space-x-3">
+              <button
+                  id="increment-${counter.id}"
+                  onclick="increment('${counter.id + 1}', '${
+        counter.increment
+      }')"
+               class="bg-indigo-400 text-white px-3 py-2 rounded shadow">
+                Increment By ${counter.increment}
+              <button 
+                  id="decrement-${initialState.counters + 1}"
+                  onclick="decrement('${counter.id + 1}', '${
+        counter.decrement
+      }')"
+              class="bg-red-400 text-white px-3 py-2 rounded shadow">
+                Decrement By ${counter.decrement}
+              </button>
+            </div>
+          </div>
+      `;
+      fullCounterEl.appendChild(newCounter);
+    });
+  };
+
+
+
+
+
+
+
+// create reducer function  
+
+function counterReducer (state= initialState,action){
+    if(action.type === ADD_COUNTER){
+            return{
+                counters:[
+                    ...state.counters,
+                ,action.payload
+                ],
+                totalCounters:state.totalCounters + 1 ,
+            }
+        
+
+        
+    }
+     if(action.type === INCREMENT){
+        return {
+            ...state,
+            counters: state.counters.map((counter) => {
+              if (counter.id + 1 === Number(action.payload.id)) {
+                return {
+                  ...counter,
+                  value: counter.value + counter.increment,
+                };
+              } else {
+                return counter;
+              }
+            }),
+          };
+        
+
+        
+    }
+
+    if(action.type === DECREMENT){
+        return {
+            ...state,
+            counters: state.counters.map((counter) => {
+              if (counter.id + 1 === Number(action.payload.id)) {
+      
+                return {
+                  ...counter,
+                  value: counter.value - counter.decrement,
+                };
+              } else {
+                return counter;
+              }
+            }),
+          };
+        
+    }
+  if(action.type === RESET){
+    return {
+        ...state,
+        counters: state.counters.map((counter) => {
+          return {
+            ...counter,
+            value: 0,
+          };
+        }),
+      };
+   }
+   
+ return state
+   
+
+}
+
+
+
+//Subscribe to store changes
+store.subscribe(() => {
+    console.log(store.getState());
+  });
+  
+  //Initial render
+  addNewCounter();
+
+// const render = ()=>{
+//     const state = store.getState();
+//     counterEl.innerText = state.value.toString();
+// }
+// render()
+
+// store.subscribe(render)
 
 
 
@@ -133,13 +247,27 @@ store.subscribe(render)
 
 // button handler  
 
-incrementEl.addEventListener("click",()=>{
-    store.dispatch(increment(5))
+addCounterEl.addEventListener("click",()=>{
+    store.dispatch(addCounter());
+    addNewCounter()
+
 })
 
-decrementEl.addEventListener("click",()=>{
-    store.dispatch(decrement(1))
-})
+
 resetEl.addEventListener("click",()=>{
-    store.dispatch(reset(2))
+    store.dispatch(reset());
+    resetAllCounter()
 })
+
+
+function incrementHandler(id, increment) {
+    store.dispatch(incrementAction(id));
+    document.getElementById(id).innerText =
+      Number(document.getElementById(id).innerText) + Number(increment);
+  }
+  
+  function decrementHandler(id, decrement) {
+    store.dispatch(decrementAction(id));
+    document.getElementById(id).innerText =
+      Number(document.getElementById(id).innerText) - Number(decrement);
+  }
